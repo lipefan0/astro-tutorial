47ef04491d586f3018a9b8c99996bf1056d7a48c
Felipe Oliveira <lipefan0@gmail.com>
2024-12-03 17:40:12 -0300

    feat: add Preact integration and update TypeScript configuration for JSX support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://resonant-pegasus-30acc2.netlify.app/"
+  site: "https://resonant-pegasus-30acc2.netlify.app/",
+  integrations: [preact()]
 });
